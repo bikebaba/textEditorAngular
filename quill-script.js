1633b99d1aa09b67734c2ddfa37fb9392925af48
Shiv Bhuvanapalli <bikebaba@yahoo.com>
2016-12-07 19:07:37 -0500

    Added Choose file and submit functionlity

--- quill-script.js
+++ quill-script.js
@@ -21,8 +21,43 @@ myAppModule.controller('AppCtrl', [
         };
         $scope.saveas = function() {
             var data = new Blob([$scope.title], { type: 'text/plain;charset=utf-8' });
-            FileSaver.saveAs(data, 'text.txt');
+            FileSaver.saveAs(data, 'text.html');
             //alert($scope.htmlContent);
         }
+        $scope.displayFileContents = function(contents) {
+            console.log("DDD");
+            $scope.title = contents;
+        };
     }
 ]);
+
+
+myAppModule.directive('onReadFile', function ($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            element.bind('change', function(e) {
+
+                var onFileReadFn = $parse(attrs.onReadFile);
+                var reader = new FileReader();
+
+                reader.onload = function() {
+                    var fileContents = reader.result;
+                    // invoke parsed function on scope
+                    // special syntax for passing in data
+                    // to named parameters
+                    // in the parsed function
+                    // we are providing a value for the property 'contents'
+                    // in the scope we pass in to the function
+                    scope.$apply(function() {
+                        onFileReadFn(scope, {
+                            'contents' : fileContents
+                        });
+                    });
+                };
+                reader.readAsText(element[0].files[0]);
+            });
+        }
+    };
+})
